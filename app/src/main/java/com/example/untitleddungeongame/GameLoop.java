ce59258b1e5af4f376ce28717da32913b2e4c02e
CactiHomie <cmonnich04@gmail.com>
2025-03-10 10:29:12 -0600

    Cole: Added a gameloop, the base class where all logic and drawing will happen

---
+++ app/src/main/java/com/example/untitleddungeongame/GameLoop.java
@@ -0,0 +1,19 @@
+package com.example.untitleddungeongame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class GameLoop extends SurfaceView implements Runnable {
+
+
+    public GameLoop(Context context) {
+        super(context);
+        //Stuff happens Here
+    }
+
+    @Override
+    public void run() {
+        //GameLoop happens Here
+
+    }
+}
