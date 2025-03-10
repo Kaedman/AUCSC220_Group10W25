60aa24e91b8f4969923a4ad0de1e4482ae4c1ed2
CactiHomie <cmonnich04@gmail.com>
2025-03-10 15:11:46 -0600

    Worked on getting visuals to work, see draw() as one issue is still happening

--- app/src/main/java/com/example/untitleddungeongame/GameLoop.java
+++ app/src/main/java/com/example/untitleddungeongame/GameLoop.java
@@ -2,6 +2,7 @@ package com.example.untitleddungeongame;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -16,15 +17,26 @@ public class GameLoop extends SurfaceView implements Runnable {
 
     //Graphics
     private Canvas canvas; //drawing happens here
-    private SurfaceHolder actualSurface; //Actual visual
+    private SurfaceHolder surfaceHolder; //Actual visual
 
     private Paint paint; //This may not be required
 
+    //Other
+    private Context context;
+
     public GameLoop(Context context) {
         super(context);
+
+        this.context = context;
         //Stuff happens Here
 
-        actualSurface = getHolder();
+        surfaceHolder = getHolder();
+        paint = new Paint();
+
+    }
+    public GameLoop(Context context, SurfaceView surfaceView){
+        this(context);
+        surfaceHolder = surfaceView.getHolder();
 
     }
 
@@ -32,24 +44,31 @@ public class GameLoop extends SurfaceView implements Runnable {
     public void run() {
         //GameLoop happens Here
 
+        System.out.println("Running loop");
+        draw();
     }
 
     /*
     Draw instructions for all visuals relevant to the game
      */
     public void draw(){
-        if (!actualSurface.getSurface().isValid()) return;//check surface is correct
 
-        canvas = actualSurface.lockCanvas(); //get the current surface as a canvas object, prevent changes to surface
+        if (!surfaceHolder.getSurface().isValid()) return;//check surface is correct
+        //currently we are stopping at this check, as our surface is not valid
+        //TODO: Get a proper working surface
+
+        canvas = surfaceHolder.lockCanvas(); //get the current surface as a canvas object, prevent changes to surface
+
         //stuff happens
+        System.out.println("Drawing");
+        paint.setColor(Color.RED);
+        canvas.drawRect(0,0,100,100, paint);
 
 
-        actualSurface.unlockCanvasAndPost(canvas); //update the surface
-
+        surfaceHolder.unlockCanvasAndPost(canvas); //update the surface
+        System.out.println("Drawing done");
 
 
     }
-
-
 
 }
