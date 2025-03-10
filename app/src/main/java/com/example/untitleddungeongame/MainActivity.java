1a7fd1c9980f7c58dd9c28104285df8575140195
CactiHomie <cmonnich04@gmail.com>
2025-03-10 15:11:10 -0600

    Added a thread and set up GameLoop

--- app/src/main/java/com/example/untitleddungeongame/MainActivity.java
+++ app/src/main/java/com/example/untitleddungeongame/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.untitleddungeongame;
 
 import android.os.Bundle;
+import android.view.Display;
+import android.view.SurfaceView;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declarations
+
+
+    SurfaceView gameView;
+    Thread gameThread;
+    GameLoop game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Initializations Work Goes here
+        gameView = findViewById(R.id.gameView);
+
+
+        //Need the apps context, as for drawing, the gameLoop is our target display
+        game = new GameLoop(this, gameView);
+
+
+
+//        setContentView(game);
+
+        gameThread = new Thread(game);
+        gameThread.start();
+
     }
 }
