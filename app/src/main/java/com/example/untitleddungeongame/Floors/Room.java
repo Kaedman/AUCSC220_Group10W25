927d3826b78f1779e92ec8ee96fa410ba86ba939
Adam <a.holmes2104@hotmail.com>
2025-03-21 16:23:25 -0600

    Added setters and roomId for room class, as well as organizing them into their own folder.

--- app/src/main/java/com/example/untitleddungeongame/Floors/Room.java
+++ app/src/main/java/com/example/untitleddungeongame/Floors/Room.java
@@ -1,18 +1,19 @@
-package com.example.untitleddungeongame;
+package com.example.untitleddungeongame.Floors;
 
 public class Room {
-    Room left;
-    Room right;
-    Room up;
-    Room down;
-
+    private Room left;
+    private Room right;
+    private Room up;
+    private Room down;
+    private int roomId;
     public Room(Room left, Room right, Room up, Room down) {
-        this.left = left;
-        this.right = left;
-        this.up = left;
-        this.down = left;
+        this.left = null;
+        this.right = null;
+        this.up = null;
+        this.down = null;
     }
 
+    //Getters
     public Room getLeft() {
         return left;
     }
@@ -28,7 +29,23 @@ public class Room {
     public Room getDown() {
         return down;
     }
+    public int getRoomId(){
+        return roomId;
+    }
 
+    //Setters
+    public void setLeftRoom(Room adjacentRoom){
+        this.left = adjacentRoom;
+    }
+    public void setRightRoom(Room adjacentRoom){
+        this.right = adjacentRoom;
+    }
+    public void setDownRoom(Room adjacentRoom){
+        this.down = adjacentRoom;
+    }
+    public void setUpRoom(Room adjacentRoom){
+        this.up = adjacentRoom;
+    }
     /**
      * Gets each of a rooms adjacent rooms (left, right, up, down) and returns them in the form of
      * an array of Rooms
