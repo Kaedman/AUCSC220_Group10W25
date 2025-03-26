36a2b690b9269e15ba5689ea191adaba0e936e6b
Kaedman <kaedenh47@gmail.com>
2025-03-26 10:09:29 -0600

    Floor Array Generation and associated Tests implemented
    Also fixed a bug where dead ends would lead to an infinite loop looking for an emptyPath

--- app/src/test/java/com/example/untitleddungeongame/RoomMasterTest.java
+++ app/src/test/java/com/example/untitleddungeongame/RoomMasterTest.java
@@ -4,10 +4,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import com.example.untitleddungeongame.RoomMaster;
-
-import android.util.Log;
-
 import org.junit.Test;
 
 import java.util.ArrayList;
