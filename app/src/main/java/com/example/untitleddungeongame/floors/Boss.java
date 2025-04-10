6e808cc5ee912733e7377e4a76e1445a2d59a2c4
CactiHomie <cmonnich04@gmail.com>
2025-04-10 12:10:42 -0600

    Committing to merge to main

--- app/src/main/java/com/example/untitleddungeongame/floors/Boss.java
+++ app/src/main/java/com/example/untitleddungeongame/floors/Boss.java
@@ -2,8 +2,7 @@ package com.example.untitleddungeongame.floors;
 
 import com.example.untitleddungeongame.Enemy;
 import com.example.untitleddungeongame.floors.Room;
-
-public class Boss extends Room {
+public class Boss extends Room{
     private final Enemy boss;
     private boolean isCleared;
     RoomMaster nextFloor;
